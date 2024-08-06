6a2e708e63ccfaa6901cbfa0f17ae5e736739c02
YoAquinjs <sebascertuche@gmail.com>
2024-08-06 12:36:19 -0600

    lua and luarocks install scritps

---
+++ scripts/lua-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Dependencies: curl, tar, make
+
+usage() {
+    echo "Usage: $0 [lua-version]"
+}
+
+if [ "$#" -gt 1 ]; then
+    usage
+    exit 1
+fi
+
+luaVersion="5.4.7"
+
+if [ "$#" -eq 1 ]; then
+   luaVersion=$1
+fi
+
+luaDir="lua-${luaVersion}"
+luaTar="${luaDir}.tar.gz"
+curl -L -R -O "https://www.lua.org/ftp/$luaTar"
+tar zxf $luaTar
+cd $luaDir
+make all test
+
+cd src
+sudo cp lua /usr/bin
+sudo cp luac /usr/bin
+sudo cp lua "/usr/bin/lua$luaVersion"
+sudo cp lua.h /usr/include
+
+cd ../..
+rm -rf $luaDir
+rm $luaTar
+
---
+++ scripts/luarocks-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Dependencies: curl, tar, make
+
+usage() {
+    echo "Usage: $0 [luarocks-version]"
+}
+
+if [ "$#" -gt 1 ]; then
+    usage
+    exit 1
+fi
+
+luarVersion="3.11.1"
+
+if [ "$#" -eq 1 ]; then
+   luarVersion=$1
+fi
+
+luarDir="luarocks-${luarVersion}"
+luarTar="${luarDir}.tar.gz"
+
+curl -L -R -O "https://luarocks.org/releases/$luarTar"
+tar zxpf $luarTar
+cd $luarDir
+./configure && make && sudo make install
+
+cd ..
+rm -rf $luarDir
+rm $luarTar
+
