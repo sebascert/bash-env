57f07fa4a4b9cecb0ef62692dd692e93a1e2a476
YoAquinjs <sebascertuche@gmail.com>
2024-11-15 21:52:31 -0600

    refactor last-file commands

--- home/functions/last-core.sh
+++ home/functions/last-core.sh
@@ -1,21 +1,4 @@
 last-core() {
-    local latest_file=""
-    local latest_time=0
-
-    for file in core.*; do
-        if [[ -f "$file" ]]; then
-            file_time=$(stat -c %W "$file")
-
-            if [[ $file_time -eq 0 ]]; then
-                file_time=$(stat -c %Y "$file")
-            fi
-
-            if [[ $file_time -gt $latest_time ]]; then
-                latest_time=$file_time
-                latest_file="$file"
-            fi
-        fi
-    done
-
-    echo "$latest_file"
+    n=${1:-0}
+    la core.* | last-file "$n"
 }
