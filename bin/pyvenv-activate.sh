4e6c3a7602d402da7b2e31aa8ff07b739f698e61
YoAquinjs <sebascertuche@gmail.com>
2025-03-01 12:47:21 -0600

    refactor pyvenvact for multiple venv names and clip debug

--- bin/pyvenv-activate.sh
+++ bin/pyvenv-activate.sh
@@ -3,23 +3,37 @@
 # Dependencies: python3, virtualenv
 
 status=""
-venv="pyvenv"
+# if no venv is found then default to first one
+venv_dir=("pyvenv" ".venv" "venv")
 
 current_dir="$(pwd)"
+venv_path=""
 
-while [[ "$current_dir" != "/" && ! -d "$current_dir/$venv" ]]; do
+while [[ "$current_dir" != "/" ]]; do
+    for dir in "${venv_dir[@]}"; do
+        if [[ -d "$current_dir/$dir" ]]; then
+            venv_path="$current_dir/$dir"
+            break
+        fi
+    done
+
+    if [[ "$venv_path" != "" ]]; then
+        break
+    fi
+
     current_dir="$(dirname "$current_dir")"
 done
 
-if [[ -d "$current_dir/$venv" ]]; then
-    status="reactivating $current_dir/$venv"
+if [[ "$venv_path" != "" ]]; then
+    status="reactivating $venv_path"
 else
-    virtualenv "$venv" > /dev/null
     current_dir="$(pwd)"
-    status="$venv created"
+    venv_path="$current_dir/${venv_dir[0]}"
+    virtualenv "$venv_path" > /dev/null
+    status="$venv_path created"
 fi
 
-source "$current_dir/$venv/bin/activate"
+source "$venv_path/bin/activate"
 
 # py_version=$("$venv/bin/python" --version | sed -n -E 's/.* ([0-9]+\.[0-9]+).*/\1/p')
 # PYTHON_VENV_PATH="$current_dir/$venv/lib/python$py_version/site-packages"
