28c3e52f422fce97bcad902acf473410b1987e83
Sebastian Certuche <sebascertuche@gmail.com>
2025-09-12 22:40:56 -0600

    Refactor new docc script

--- bin/enterdocc.sh
+++ bin/enterdocc.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+nwin() {
+    tmux new-window -n "$new_docc"
+    tmux split-window -h
+    tmux resize-pane -R 16
+    tmux send-keys -t "$new_docc".0 "$1" C-m
+}
+
 if [ $# -ne 1 ]; then
     echo "missing doc name"
     exit 1
@@ -10,13 +17,14 @@ fi
 new_docc="$1"
 
 docc_dir="$HOME/docc"
+mkdir -p "$docc_dir"
 
 if [ -d "$docc_dir/$new_docc" ]; then
-    echo "existing doc"
-    exit 1
+    cd "$docc_dir/$new_docc"
+    nwin "git status"
+    exit 0
 fi
 
-mkdir -p "$docc_dir"
 cd "$docc_dir"
 
 git clone git@github.com:sebascert/docc.git "$new_docc"
@@ -26,8 +34,4 @@ cd "$new_docc" || {
     exit 2
 }
 
-on_new_win="nvim src/body.md"
-tmux new-window -n "$new_docc" "$on_new_win" \; split-window -h \; resize-pane -R 16 || {
-    echo "new tmux window failed"
-    exit 3
-}
+nwin "nvim src/body.md"
